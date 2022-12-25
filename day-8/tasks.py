8ca3c3af22a08225ea65ef33a93ff8d566e00e13
decoder247 <20130935+decoder247@users.noreply.github.com>
2022-12-24 22:33:52 -0500

    day8: WIP

--- day-8/tasks.py
+++ day-8/tasks.py
@@ -1,5 +1,5 @@
 """
-xxx
+Sliding window algorithm to determine 'visibility'
 """
 
 from collections import deque
@@ -8,10 +8,21 @@ from os.path import join
 from pathlib import Path
 from sys import argv
 from textwrap import dedent
-from typing import Tuple, Optional
+from typing import Optional, Tuple, List
+
+import numpy as np
 
 from src.timer import Timer
 from src.utils import read_text_file
+
+
+def string_to_int_list(string: str) -> List[int]:
+    return [int(char) for char in list(string)]
+
+
+def get_elements_visible_in_grid(grid: np.array) -> List[Tuple]:
+    
+    outer_elements_visible = $$
 
 
 def main():
@@ -21,22 +32,24 @@ def main():
     input_fname = "input.txt" if not test_mode else "sample.txt"
 
     lines = read_text_file(join(parent_dir, input_fname))
+    gridspace = np.array([string_to_int_list(l) for l in lines], dtype=int)
 
     DEBUG = True
 
     with Timer(name=timer_name, return_ns=True, ns_mode=True) as timer:
-        print(lines)
-        time_1_microsecs = timer.stop(reset=True) / 1000
-        total_time_microsecs = timer.timers[timer_name] * (10**6)
+        visible_coords = get_elements_visible_in_grid(grid=gridspace)
+        time_1_millisecs = timer.stop(reset=True) / 1000000
+        total_time_millisecs = timer.timers[timer_name] * (10**3)
 
     # Print out results
+    print(gridspace)
     print(
         dedent(
             f"""
-    Task 1 ({time_1_microsecs:.1f}us elapsed):
+    Task 1 ({time_1_millisecs:.2f}ms elapsed):
     XXX
 
-    Total time taken: {total_time_microsecs:.1f}us
+    Total time taken: {total_time_millisecs:.2f}ms
     """
         )
     )
